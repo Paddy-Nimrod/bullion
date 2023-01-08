49eba0fe19112862db74b6df5732adc83c6f890e
Paddy-Nimrod <n.ambetsa21@gmail.com>
2023-01-08 17:10:04 +0300

    first commit

---
+++ components/header.js
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div>
+      <div className="flex items-center font-bold text-sm md:text-xl lg:text-2xl p-5 bg-slate-900 text-white">
+        Bullion Trading
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ components/homeComponent.js
@@ -0,0 +1,88 @@
+const HomeComponent = () => {
+  return (
+    <>
+      <div className="p-5 md:p-11 lg:p-11 md:w-4/5 lg:w-4/5 m-auto">
+        <table class="coin-table table-auto w-full">
+          <thead className="bg-slate-900">
+            <tr>
+              <th>coin name</th>
+              <th>Bullion Trading</th>
+              <th>APMEX</th>
+              <th>JMBullion</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1oz American Eagle Gold Coin</td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>1oz Gold Bar Pamp Suisse Lady</td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>1 oz African Kruggerand gold coin</td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>1 oz Canadian Maple leaf gold coin</td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>1 Kilo gold bar</td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>1 oz Gold Bars Random</td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>1 oz American Eagle Silver</td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>100 oz Silver Bar</td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>1 oz Silver Bars</td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>1 oz Silver Rounds</td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>100 oz Silver Bar Pamp</td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default HomeComponent;
